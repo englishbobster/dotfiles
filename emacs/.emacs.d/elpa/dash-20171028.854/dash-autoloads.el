33c160a643af04026c8df5eb1fd42a995186a3ab
Stuart Osborne <stuart.osborne@infinera.com>
2018-02-24 20:41:57 +0100

    Probably dont need emacs.d since it will be rebuilt from need.!

--- emacs/.emacs.d/elpa/dash-20171028.854/dash-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("dash.el") (23056 16808 19784 72000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
--- emacs/.emacs.d/elpa/tomatinho-20161023.1853/tomatinho-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; tomatinho-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "tomatinho" "tomatinho.el" (22598 64196 844369
-;;;;;;  564000))
-;;; Generated autoloads from tomatinho.el
-
-(autoload 'tomatinho "tomatinho" "\
-A simple and beautiful pomodoro technique timer.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("tomatinho-pkg.el") (22598 64196 853381
-;;;;;;  74000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; tomatinho-autoloads.el ends here
