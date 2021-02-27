c274427cf8f137738c3a1b672de7547e45e7bd12
Stuart Osborne <stuart.osborne@pricer.com>
2021-02-27 14:07:52 +0100

    removed redundant screenlayouts.

--- screenlayout/.screenlayout/proj.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output VGA1 --mode 1024x768 --pos 0x0 --rotate normal --output HDMI1 --off --output eDP1 --mode 1024x768 --output HDMI2 --off
--- screenlayout/.screenlayout/vnc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VNC-0 --mode 1920x1080 --pos 0x0 --rotate normal
